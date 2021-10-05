6428f1afedf538cef5441a4443fb371a7a34d296
Tamas Komoroczy <szolarisz@gmail.com>
2021-10-05 10:42:13 +0200

    Első node.js szerverem.

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const port = 4444;
+const fs = require('fs');
+
+const server = http.createServer( (req,res) =>{
+    res.write(`A kérés forrása: ${req.url}`);
+    res.end("      -Vége");
+})
+
+server.listen(port);
