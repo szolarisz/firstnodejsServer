88126c79fc2a83fee55435cec91568f9109b0824
Tamas Komoroczy <szolarisz@gmail.com>
2021-10-05 14:55:43 +0200

    Előkészítve JSON kommunikációra

--- index.js
+++ index.js
@@ -2,34 +2,37 @@ const http = require('http');
 const port = 4444;
 const fs = require('fs');
 
-const server = http.createServer( (req,res) =>{
-   
+const server = http.createServer((req, res) => {
+
     console.log(req.url);
     console.log(req.method);
-    
-    
+
+
     //router
-    switch (true){
+    switch (true) {
         case req.url === '/' && req.method === 'GET':
-            fs.readFile('./views/index.html', (err,file) =>{
+            fs.readFile('./views/index.html', (err, file) => {
                 res.setHeader('Content-Type', 'text/html');
                 res.end(file);
             });
             break;
-            case req.url === '/minta.css' && req.method === 'GET':
-                fs.readFile('./views/minta2.css', (err,file) =>{
-                    res.setHeader('Content-Type', 'text/css');
-                    res.end(file);
-                });
-                break;    
+        case req.url === '/minta.css' && req.method === 'GET':
+            fs.readFile('./views/minta2.css', (err, file) => {
+                res.setHeader('Content-Type', 'text/css');
+                res.end(file);
+            });
+            break;
+        case req.url === '/telefon' && req.method === 'GET':
+            kiszolgal('./telefon.json', 'application/json', req, res);
+            break;
         default:
-            kiszolgal('./views/hiba404.html','text/html',req,res);
+            kiszolgal('./views/hiba404.html', 'text/html', req, res);
             //Majd ezt is kuldom állományból    
     }
 })
 
-function kiszolgal(filename, type, req, res){
-    fs.readFile(filename, (err,file) =>{
+function kiszolgal(filename, type, req, res) {
+    fs.readFile(filename, (err, file) => {
         res.setHeader('Content-Type', type);
         res.end(file);
     });
