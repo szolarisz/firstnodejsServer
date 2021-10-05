28ed802d8334793b3a74274c70ed7331327571da
Tamas Komoroczy <szolarisz@gmail.com>
2021-10-05 10:51:16 +0200

    kibővítve a headers kiírásával

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const port = 4444;
 const fs = require('fs');
 
 const server = http.createServer( (req,res) =>{
+    res.write(req.headers);
     res.write(`A kérés forrása: ${req.url}`);
     res.end("      -Vége");
 })
+
+
 
 server.listen(port);
