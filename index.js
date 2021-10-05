2e441a66dd9978d4ab940d7db27f9ebdb4ac2990
Tamas Komoroczy <szolarisz@gmail.com>
2021-10-05 14:45:47 +0200

    router több kérés kiszolgálására - alap helyzet

--- index.js
+++ index.js
@@ -3,9 +3,32 @@ const port = 4444;
 const fs = require('fs');
 
 const server = http.createServer( (req,res) =>{
-    res.write(req.headers);
-    res.write(`A kérés forrása: ${req.url}`);
-    res.end("      -Vége");
+   
+    console.log(req.url);
+    console.log(req.method);
+    
+    
+    //router
+    switch (true){
+        case req.url === '/' && req.method === 'GET':
+            fs.readFile('./views/index.html', (err,file) =>{
+                res.setHeader('Content-Type', 'text/html');
+                res.end(file);
+            });
+            break;
+            case req.url === '/minta.css' && req.method === 'GET':
+                fs.readFile('./views/minta2.css', (err,file) =>{
+                    res.setHeader('Content-Type', 'text/css');
+                    res.end(file);
+                });
+                break;    
+        default:
+            fs.readFile('./views/hiba404.html', (err,file) =>{
+                res.setHeader('Content-Type', 'text/html');
+                res.end(file);
+            });
+            //Majd ezt is kuldom állományból    
+    }
 })
 
 
